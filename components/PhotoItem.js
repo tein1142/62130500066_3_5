c1a5ac133bcdad2c54f4cfae5a4c4d52c4da2b49
tein1142 <tein1142@hotmail.com>
2021-02-24 19:29:59 +0700

    [ADD] assignment5

--- components/PhotoItem.js
+++ components/PhotoItem.js
@@ -1,41 +1,41 @@
 app.component('photo-item', {
   props: {
     pic: Object
-  }
-  ,
-  tamplate: `<div class="ml-12 flex flex-col">
-    <div>
-      <ul>
-        <li>
-          <button @click="canvasShow(pic.image)" v-on:dblclick="doneToggle(index)">
-           <a href="#likePic"><img v-bind:src="pic.image" class="mt-8 hover:shadow " width="250"></a> 
-          </button>
-          <p class="font-bold text-blue-800">{{pic.title}}
-            <button v-on:click="doneToggle(pic.title)">
-              <span v-show="!pic.done" class="material-icons text-gray-300 text-2xl">
-                favorite
-              </span>
-              <span v-show="pic.done" class="material-icons text-red-500 text-2xl">
-                favorite
-              </span>
-            </button>
-          </p>
-          <p>{{pic.description}}</p>
-          <p>{{pic.description2}}</p>
-    </div>
+  },
 
-
+  template:
+    /*html*/
+    `
+  <div class="ml-12 flex flex-col">
+  <ul>
+    <li>
+      <button @click="showCanvas(pic.image)" v-on:dblclick="toggleDone(index)">
+        <a href="#likePic"><img v-bind:src="pic.image" class="mt-8 hover:shadow " width="250"></a>
+      </button>
+      <p class="font-bold text-blue-800">{{pic.title}}
+        <button v-on:click="toggleDone(pic.title)">
+          <span v-show="!pic.done" class="material-icons text-gray-300 text-2xl">
+            favorite
+          </span>
+          <span v-show="pic.done" class="material-icons text-red-500 text-2xl">
+            favorite
+          </span>
+        </button>
+      </p>
+      <p>{{pic.description}}</p>
+      <p>{{pic.description2}}</p>
     </li>
-    </ul>
-  </div> `,
+  </ul>
+</div>
+  `,
   methods: {
-    picSearch() {
-      this.$emit('search-pic')
-    },
-    canvasShow(picture) {
+    // picSearch() {
+    //   this.$emit('search-pic')
+    // },
+    showCanvas(picture) {
       this.$emit('show-canvas', picture)
     },
-    doneToggle(text) {
+    toggleDone(text) {
       this.$emit('toggle-done', text)
     }
 
